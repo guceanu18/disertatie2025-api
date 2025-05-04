a17a6e46eca352b2d2c615643152b9a8644bfd97
guceanu18 <guceanudaniel18@gmail.com>
2025-05-04 13:49:01 +0300

    feat: support for provision_multicast

--- src/client/client_test.py
+++ src/client/client_test.py
@@ -12,24 +12,24 @@ print(client.add_router(
 # List routers
 print(client.list_routers())
 
-# Get a router by name
-print(client.get_router_by_name("R_INTERNET"))
-
-# Update a router
-print(client.update_router("R_INTERNET", mgmt_ip="192.168.72.128"))
+# # Get a router by name
+# print(client.get_router_by_name("R_INTERNET"))
+#
+# # Update a router
+# print(client.update_router("R_INTERNET", mgmt_ip="192.168.72.128"))
 
 # Send a command to a router
 print(client.run_command("R_INTERNET", "show ip interface brief"))
 
-# Push a config to the router
-print(client.push_config(
-    router_name="R_INTERNET",
-    template_name="add_router.j2",
-    template_vars={
-        "mgmt_ip": "1.1.1.1",
-        "mgmt_lan_ip": "2.2.2.2"
-    }
-))
+# # Push a config to the router
+# print(client.push_config(
+#     router_name="R_INTERNET",
+#     template_name="add_router.j2",
+#     template_vars={
+#         "mgmt_ip": "1.1.1.1",
+#         "mgmt_lan_ip": "2.2.2.2"
+#     }
+# ))
 
 
 # Delete a router
